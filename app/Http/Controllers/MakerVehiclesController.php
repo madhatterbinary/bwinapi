9caa824600bcf4d6b080cbfdd950324e32fb10bd
madhatterbinary <ulysses@mojitocoder.com>
2016-08-08 22:50:35 +0100

    Creating of controllers
    Creating controllers for every route to API

--- app/Http/Controllers/MakerVehiclesController.php
+++ app/Http/Controllers/MakerVehiclesController.php
@@ -1,15 +1,12 @@
 <?php namespace App\Http\Controllers;
+
 use App\Http\Requests;
 use App\Http\Controllers\Controller;
+
 use Illuminate\Http\Request;
-use App\Maker;
-use App\Vehicle;
-use App\Http\Requests\CreateVehicleRequest;
+
 class MakerVehiclesController extends Controller {
-	public function __construct()
-	{
-		$this->middleware('oauth', ['except' => ['index', 'show']]);
-	}
+
 	/**
 	 * Display a listing of the resource.
 	 *
@@ -17,97 +14,50 @@ class MakerVehiclesController extends Controller {
 	 */
 	public function index($id)
 	{
-		$maker = Maker::find($id);
-		if(!$maker)
-		{
-			return response()->json(['message' => 'This maker does not exist', 'code' => 404], 404);
-		}
-		return response()->json(['data' => $maker->vehicles], 200);
+		return "'Showing the vehicles of $id";
 	}
+
 	/**
 	 * Store a newly created resource in storage.
 	 *
 	 * @return Response
 	 */
-	public function store(CreateVehicleRequest $request, $makerId)
+	public function store()
 	{
-		$maker = Maker::find($makerId);
-		if(!$maker)
-		{
-			return response()->json(['message' => 'This maker does not exist', 'code' => 404], 404);
-		}
-		$values = $request->all();
-		$vehicle = $maker->vehicles()->create($values);
-		return response()->json(['message' => "The vehicle associated was created with serie: {$vehicle->serie}"], 201);
+		//
 	}
+
 	/**
 	 * Display the specified resource.
 	 *
 	 * @param  int  $id
 	 * @return Response
 	 */
-	public function show($id, $vehicleId)
+	public function show($id)
 	{
-		$maker = Maker::find($id);
-		if(!$maker)
-		{
-			return response()->json(['message' => 'This maker does not exist', 'code' => 404], 404);
-		}
-		$vehicle = $maker->vehicles->find($vehicleId);
-		if(!$vehicle)
-		{
-			return response()->json(['message' => 'This vehicle does not exist for this maker', 'code' => 404], 404);
-		}
-		return response()->json(['data' => $vehicle], 200);
+		return "'Showing the vehicles of $id";
 	}
+
 	/**
 	 * Update the specified resource in storage.
 	 *
 	 * @param  int  $id
 	 * @return Response
 	 */
-	public function update(CreateVehicleRequest $request, $makerId, $vehicleId)
+	public function update($id)
 	{
-		$maker = Maker::find($makerId);
-		if(!$maker)
-		{
-			return response()->json(['message' => 'This maker does not exist', 'code' => 404], 404);
-		}
-		$vehicle = $maker->vehicles->find($vehicleId);
-		if(!$vehicle)
-		{
-			return response()->json(['message' => 'This vehicle does not exist', 'code' => 404], 404);
-		}
-		$color = $request->get('color');
-		$power = $request->get('power');
-		$capacity = $request->get('capacity');
-		$speed = $request->get('speed');
-		$vehicle->color = $color;
-		$vehicle->power = $power;
-		$vehicle->capacity = $capacity;	
-		$vehicle->speed = $speed;
-		$vehicle->save();
-		return response()->json(['message' => 'The vehicle has been updated'], 200);
+		//
 	}
+
 	/**
 	 * Remove the specified resource from storage.
 	 *
 	 * @param  int  $id
 	 * @return Response
 	 */
-	public function destroy($makerId, $vehicleId)
+	public function destroy($id)
 	{
-		$maker = Maker::find($makerId);
-		if(!$maker)
-		{
-			return response()->json(['message' => 'This maker does not exist', 'code' => 404], 404);
-		}
-		$vehicle = $maker->vehicles->find($vehicleId);
-		if(!$vehicle)
-		{
-			return response()->json(['message' => 'This vehicle does not exist', 'code' => 404], 404);
-		}
-		$vehicle->delete();
-		return response()->json(['message' => 'The vehicle has been deleted'], 200);
+		//
 	}
+
 }
--- app/Http/routes.php
+++ app/Http/routes.php
@@ -10,9 +10,8 @@
 | and give it the controller to call when that URI is requested.
 |
 */
-Route::resource('makers', 'MakerController', ['except' => ['create', 'edit']]);
+	Route::resource('makers', 'MakerController', ['except' => ['create', 'edit']]);
+	Route::resource('vehicles', 'VehicleController', ['only' => ['index']]);
+	Route::resource('makers.vehicles', 'MakerVehiclesController', ['except' => ['edit', 'create']]);
 
-Route::resource('vehicles', 'VehiclesController', ['only' => ['index']]);
-
-Route::resource('makers.vehicles', 'MakerVehiclesController', ['except' => ['edit', 'create']]);
 
